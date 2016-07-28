3dabd8c29d56534b3f3993bd4552bab9d938ab50
Vincent <vincentckhuang@gmail.com>
2016-07-28 10:49:25 -0700

    updated devise model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
 	has_many :pets, dependent: :destroy
 
 	has_many :followers, dependent: :destroy
