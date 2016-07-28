7487e0a437c34ec0ec4444b9542613c32da434a6
Vincent <vincentckhuang@gmail.com>
2016-07-28 10:03:13 -0700

    moved commented code around in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -28,7 +28,47 @@ class UsersController < ApplicationController
       end
     end
 
+    @pets.each do |pet|
+      test = true
+      @following_pets.each do |following|
+        if following.id == pet.id
+          test = false
+        end
+      end
+      if test
+        albums = Album.where(pet_id: pet.id)
+        if albums.length != 0
+          photos = Photo.where(album_id: albums[0].id)
+          if photos.length != 0
+            @photo_array << photos[0]
+            @pet_array << pet
+          end
+        end
+      end
+    end
 
+  end
+
+end
+
+    # # sort out pets without albums
+    # while @pet_array.length < 50 and @pet_array.length < @pets.length do
+    #   @pets.each do |pet|
+    #     albums = Album.where(pet_id: pet.id)
+    #     if albums.length > 0
+    #       albums.each do |album|
+    #         photos = Photo.where(album_id: album.id)
+    #         if photos.length > 0
+    #           @photo_array << photos[0]
+    #           @pet_array << pet
+    #         end
+    #       end
+    #     end
+    #   end
+    # end
+
+
+    # # while loop to randomize pets show in main page
     # while @pet_array.length < 50 and @pet_array.length < (@pets.length-@following_pets.length) do
     #   @test = true
     #   @num = rand(@pets.length).to_i
@@ -54,50 +94,6 @@ class UsersController < ApplicationController
     #       @photo_array << Photo.where(album_id: @album[0].id)[0]
     #       @pet_array << @pets[@num]
     #       @pet_indexes << @num
-    #     end
-    #   end
-    # end
-
-    @pets.each do |pet|
-      test = true
-      @following_pets.each do |following|
-        if following.id == pet.id
-          test = false
-        end
-      end
-      if test
-        albums = Album.where(pet_id: pet.id)
-        if albums.length != 0
-          photos = Photo.where(album_id: albums[0].id)
-          if photos.length != 0
-            @photo_array << photos[0]
-            @pet_array << pet
-          end
-        end
-      end
-    end
-
-  end
-
-end
-    # @pets.each do |pet|
-    #   if user.following.include?(pet)
-    #     @following << pet
-    #   end
-    # end
-
-    #sort out pets without albums
-    # while @pet_array.length < 50 and @pet_array.length < @pets.length do
-    #   @pets.each do |pet|
-    #     albums = Album.where(pet_id: pet.id)
-    #     if albums.length > 0
-    #       albums.each do |album|
-    #         photos = Photo.where(album_id: album.id)
-    #         if photos.length > 0
-    #           @photo_array << photos[0]
-    #           @pet_array << pet
-    #         end
-    #       end
     #     end
     #   end
     # end
