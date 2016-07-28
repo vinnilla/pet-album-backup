ca408317adb6591ff735d68a7ae9d3d15c45fe52
Vincent <vincentckhuang@gmail.com>
2016-07-28 10:24:42 -0700

    testing production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
   config.assets.compile = false
 
   #HEROKU
-
+  #why no work?
   config.assets.initialize_on_precompile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
